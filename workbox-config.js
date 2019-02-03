56cbb5164051f852a56c669748f792cc6f3f785a
boxp <tiyotiyouda@gmail.com>
2019-02-04 00:24:10 +0900

    キャッシュの破棄を優先してキャッシュ戦略を見直し

--- workbox-config.js
+++ workbox-config.js
@@ -10,7 +10,7 @@ module.exports = {
   "runtimeCaching": [
       {
           "urlPattern": /\//,
-          "handler": "staleWhileRevalidate",
+          "handler": "networkFirst",
           "options": {
               "cacheableResponse": {
                   "statuses": [0, 200],
