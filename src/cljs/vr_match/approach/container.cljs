b9f4720d409fd82f41ade640ecc9a729350acf58
boxp <tiyotiyouda@gmail.com>
2019-03-19 11:36:06 +0900

    ダミーのユーザーの数を増やす

--- src/cljs/vr_match/approach/container.cljs
+++ src/cljs/vr_match/approach/container.cljs
@@ -28,24 +28,27 @@
      (swap! mock-approach-state
             #(-> %
                  (assoc :cardItems
-                        [{:id 1
-                          :title "サンプル画像"
-                          :userName "一箱"
-                          :introduction "バーチャル清楚系女子高校生Webアプリケーションエンジニアおじさんです。こっそりプログラミングしてます。"
-                          :platForms [{:id 1 :name "VRChat"} {:id 2 :name "VRoidHub"} {:id 3 :name "VirtualCast"}]
-                          :image "https://storage.googleapis.com/boxp-tmp/profile_sample.jpg"}
-                         {:id 2
-                          :title "サンプル画像"
-                          :userName "ヒマリ"
-                          :introduction "一箱さんちのヒマリです！"
-                          :platForms [{:id 1 :name "VRChat"} {:id 3 :name "VirtualCast"}]
-                          :image "https://storage.googleapis.com/boxp-tmp/profile_sample_2.jpg"}
-                         {:id 3
-                          :title "サンプル画像"
-                          :userName "アリシア・ソリッド"
-                          :introduction "ニコニ立体で公式キャラクターやってます。よろしくお願いします！"
-                          :platForms [{:id 3 :name "VirtualCast"}]
-                          :image "https://storage.googleapis.com/boxp-tmp/profile_sample_3.jpg"}])
+                        (->>
+                          [{:id 1
+                            :title "サンプル画像"
+                            :userName "一箱"
+                            :introduction "バーチャル清楚系女子高校生Webアプリケーションエンジニアおじさんです。こっそりプログラミングしてます。"
+                            :platForms [{:id 1 :name "VRChat"} {:id 2 :name "VRoidHub"} {:id 3 :name "VirtualCast"}]
+                            :image "https://storage.googleapis.com/boxp-tmp/profile_sample.jpg"}
+                           {:id 2
+                            :title "サンプル画像"
+                            :userName "ヒマリ"
+                            :introduction "一箱さんちのヒマリです！"
+                            :platForms [{:id 1 :name "VRChat"} {:id 3 :name "VirtualCast"}]
+                            :image "https://storage.googleapis.com/boxp-tmp/profile_sample_2.jpg"}
+                           {:id 3
+                            :title "サンプル画像"
+                            :userName "アリシア・ソリッド"
+                            :introduction "ニコニ立体で公式キャラクターやってます。よろしくお願いします！"
+                            :platForms [{:id 3 :name "VirtualCast"}]
+                            :image "https://storage.googleapis.com/boxp-tmp/profile_sample_3.jpg"}]
+                          cycle
+                          (take 1000)))
                  (assoc :me {:id 1
                              :title "サンプル画像"
                              :userName "一箱"
