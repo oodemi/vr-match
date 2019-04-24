735e12acb80db2677bdee0482b9179bb0b0715ba
boxp <tiyotiyouda@gmail.com>
2019-04-25 03:10:18 +0900

    CSPを考慮してレスポンスヘッダを修正

--- api/src/vr_match_api/app/my_webapp/endpoint.clj
+++ api/src/vr_match_api/app/my_webapp/endpoint.clj
@@ -1,24 +1,43 @@
 (ns vr-match-api.app.my-webapp.endpoint
   (:require [com.stuartsierra.component :as component]
-            [compojure.core :refer [defroutes context GET POST routes]]
+            [compojure.core :refer [defroutes context GET POST OPTIONS routes]]
             [compojure.route :as route]
             [ring.adapter.jetty :as server]
-            [ring.middleware.json :refer [wrap-json-params
-                                          wrap-json-response
-                                          wrap-json-body]]
             [vr-match-api.app.my-webapp.handler :as handler]))
+
+(defn wrap-header-csp
+  [handler]
+  (fn [request]
+    (assoc-in (handler request)
+              [:headers "Content-Security-Policy"]
+              "default-src http://localhost:8888")))
+
+(defn wrap-header-cors
+  [handler]
+  (fn [request]
+    (-> (handler request)
+        (assoc-in [:headers "Access-Control-Allow-Origin"] "http://localhost:8888")
+        (assoc-in [:headers "Access-Control-Allow-Credentials"] "true"))))
 
 (defn main-routes
   [{:keys [my-webapp-handler] :as comp}]
   (routes
-    (GET "/" [req] (handler/index my-webapp-handler))
-    (GET "/graphql" [req] (handler/graphql my-webapp-handler req))
-    (route/not-found "<h1>404 page not found</h1>")))
+   (GET "/" [] (handler/index my-webapp-handler))
+   (POST "/graphql" req (handler/graphql my-webapp-handler req))
+   (OPTIONS "*" []
+     {:status 200
+      :headers {"Access-Control-Allow-Methods" "POST, GET, OPTIONS"
+                "Access-Control-Allow-Credentials" "true"
+                "Access-Control-Allow-Headers" "Content-Type"}})
+   (route/not-found {:status 404
+                     :headers {}
+                     :body "<h1>404 page not found</h1>"})))
 
 (defn app
   [comp]
   (-> (main-routes comp)
-      (wrap-json-body {:keywords? true :bigdecimals? true})))
+      wrap-header-csp
+      wrap-header-cors))
 
 (defrecord MyWebappEndpointComponent [port server my-webapp-handler]
   component/Lifecycle
