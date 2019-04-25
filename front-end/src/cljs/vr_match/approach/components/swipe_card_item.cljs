30853b171d0fe157110bea14747786344ef7966f
boxp <boxp@LAPTOP-TQC196GA.localdomain>
2019-04-25 23:17:38 +0900

    スワイプカード(単数)のコンポーネントを作成

---
+++ front-end/src/cljs/vr_match/approach/components/swipe_card_item.cljs
@@ -0,0 +1,54 @@
+(ns vr-match.approach.components.swipe-card-item
+  (:require [reagent.core :as r]
+            [vr-match.lib.components.material-ui :as mui]
+            [vr-match.lib.components.plat-form-chip :refer [plat-form-chip]]))
+
+(defn swipe-card-item-component
+  [{:keys [classes
+           item
+           handleClickCard]
+    :as props}]
+  (let [{:keys [id
+                title
+                userName
+                introduction
+                platForms
+                image]}
+        (js->clj item :keywordize-keys true)]
+     [mui/card {:style {"width" "86vw"
+                        "height" "64vh"
+                        "position" "relative"}}
+      [mui/card-action-area {:style {"width" "86vw"
+                                     "height" "100%"
+                                     "position" "relative"
+                                     "display" "flex"
+                                     "flexDirection" "column"}
+                             :on-click #(handleClickCard id)}
+       [mui/card-media {:style {"objectFit" "cover"
+                                "width" "86vw"
+                                "height" 284
+                                "flexGrow" 2}
+                        :component "div"
+                        :alt title
+                        :image (first image)
+                        :title title}]
+       [mui/card-content {:style {"width" "100%"
+                                  "boxSizing" "border-box"
+                                  "flexGrow" 1}}
+        [mui/grid {:container true
+                   :justify "flex-start"
+                   :style {"marginBottom" "0.35em"}
+                   :spacing 8}
+         (map (fn [{:keys [id name]}] [mui/grid {:key id
+                                                 :item true}
+                                       [plat-form-chip {:name name}]])
+              platForms)]
+        [mui/typo-graphy {:gutterBottom true
+                          :variant "subheading"
+                          :component "h2"}
+         userName]
+        [mui/typo-graphy {:noWrap true
+                          :style {:width "72vw"}}
+         introduction]]]]))
+
+(def card-item card-item-component)
